ab4fc50cd5fc6e04bb24c2bbcb0ec3fc6915c231
Rafael SR <raf.rsr@gmail.com>
2016-02-10 20:44:20 -0500

    fix extension configuration name

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('Rafrsr_doctrine_extra')->children();
+        $rootNode = $treeBuilder->root('rafrsr_doctrine_extra')->children();
 
         //encrypt
         $encrypt = $rootNode->arrayNode('encrypt')->addDefaultsIfNotSet()->children();
